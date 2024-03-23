cce3b2dd0f613a42a4e57ccb9610c65855cfe1e8
Kristian <10221425@ucn.dk>
2024-03-23 09:12:23 +0100

    Front Page Edit

--- resources/views/components/main-header.blade.php
+++ resources/views/components/main-header.blade.php
@@ -6,16 +6,24 @@
         viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
-      <span class="ml-3 text-xl">Tailblocks</span>
+      <span class="ml-3 text-xl">Classified</span>
     </a>
     <nav class="flex flex-wrap items-center justify-center text-base md:ml-auto">
-      <a class="mr-5 hover:text-gray-900">First Link</a>
-      <a class="mr-5 hover:text-gray-900">Second Link</a>
-      <a class="mr-5 hover:text-gray-900">Third Link</a>
-      <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+      <a class="mr-5 hover:text-gray-900">Home</a>
+      <a class="mr-5 hover:text-gray-900">All Ads</a>
+      <a class="mr-5 hover:text-gray-900">Stores</a>
+      <a class="mr-5 hover:text-gray-900">Contact</a>
+      @guest
+        <a class="mr-5 hover:text-gray-900">Login</a>
+        <a class="mr-5 hover:text-gray-900">Register</a>
+      @endguest
+      @auth
+        <a class="mr-5 hover:text-gray-900">{{ auth()->user()->name }}</a>
+      @endauth
     </nav>
     <button
-      class="mt-4 inline-flex items-center rounded border-0 bg-gray-100 px-3 py-1 text-base hover:bg-gray-200 focus:outline-none md:mt-0">Button
+      class="mt-4 inline-flex items-center rounded border-0 bg-green-500 px-3 py-1 text-base font-semibold text-gray-50 hover:bg-green-400 focus:outline-none md:mt-0">Post
+      New Ad
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
         class="ml-1 h-4 w-4" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
